efdd13fd49c1b318d568b3b3ca35fcb04f50175e
shafhan <shafhan.fa@gmail.com>
2021-03-05 06:38:27 +0700

    Using composer for require

---
+++ sprint-5/Tugas-3/src/Persegi.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Operation;
+
+class Persegi 
+{
+    public function persegi($panjang, $lebar)
+    {
+        if (isset($_POST['keliling_persegi']))
+        {
+            return (2 * $panjang) + (2 * $lebar);
+        } elseif (isset($_POST['luas_persegi']))
+        {
+            return $panjang * $lebar;
+        }
+    }
+}
